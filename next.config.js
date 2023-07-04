0cb33d1a25da2c1c801aa6cba9a5ebb6302f57a5
lleckron <leckmanbaseball@gmail.com>
2023-07-04 05:28:58 -0400

    revert

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = {
     webpack(config, { isServer }) {
         const prefix = config.assetPrefix ?? config.basePath ?? '';
         config.module.rules.push({
-          test: /\.mp4$\.png$/, 
+          test: /\.mp4$/, 
           use: [{
             loader: 'file-loader',
             options: {
